a60455ede17c528b305dba55488d10c7053051a4
Jeff Zivkovic <jzivkovic1611@gmail.com>
2020-04-19 22:07:21 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', socket => {
+    console.log('A user connected.');
+    io.emit('chat message', 'A user connected');
+    socket.on('disconnect', () => {
+        console.log('User disconnected.');
+    });
+    socket.on('chat message', msg => {
+        console.log('message: ' + msg);
+        io.emit('chat message', msg);
+    });
+});
+
+http.listen(3000, () => {
+    console.log('Listening on *:3000');
+});
